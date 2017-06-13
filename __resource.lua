9a39446de3002d7ffb5bae57cc47a85b27944c5e
samuelds <samuelds.pro@gmail.com>
2017-06-13 11:38:09 +0200

    Change events names

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T23:20:57+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-12T16:53:46+02:00
+-- @Last modified time: 2017-06-13T11:29:38+02:00
 -- @License: GNU General Public License v3.0
 
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
--- cl_functions.lua
+++ cl_functions.lua
@@ -1,9 +1,9 @@
 -- @Date:   2017-06-11T23:20:54+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-12T17:23:19+02:00
+-- @Last modified time: 2017-06-13T11:13:13+02:00
 -- @License: GNU General Public License v3.0
 
-AddEventHandler("ft_animation:Finish", function()
+AddEventHandler("ft_animation:ClFinish", function()
   Citizen.CreateThread(function()
 
     exports.ft_menuBuilder:Freeze(false)
