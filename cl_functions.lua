aad10f5057402eab197d1c11b249a2b8da20bf07
samuelds <samuelds.pro@gmail.com>
2017-06-14 21:44:36 +0200

    Update

--- cl_functions.lua
+++ cl_functions.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T23:20:54+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-13T11:13:13+02:00
+-- @Last modified time: 2017-06-13T21:31:45+02:00
 -- @License: GNU General Public License v3.0
 
 AddEventHandler("ft_animation:ClFinish", function()
--- cl_init.lua
+++ cl_init.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T23:20:51+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-11T23:20:52+02:00
+-- @Last modified time: 2017-06-14T21:33:39+02:00
 -- @License: GNU General Public License v3.0
 
 -- Load client at 100%
@@ -12,7 +12,7 @@ Citizen.CreateThread(function()
     if NetworkIsSessionStarted() then
 
       -- Send menu to Menu Generator
-      exports.ft_menuBuilder:MenuGenerator(menu)
+      exports.ft_menuBuilder:Generator(menu)
       break
 
     end
